29631af8f6236936bd4b4d0abf3bd487ab160e4a
tusk2019 <53996136+tusk2019@users.noreply.github.com>
2020-05-06 16:31:45 +0900

    Add: メソッドの追加
    顧客名検索機能実装に必要なsearch()メソッドを追加しました。

--- application/controllers/ClientTableC.php
+++ application/controllers/ClientTableC.php
@@ -17,4 +17,20 @@ class ClientTableC extends CI_Controller
         $result['shop_name'] =$this->ClientTable_model->get_shop_name();
         $this->load->view('ClientTable_view', $result);
     }
+	
+	public function search()
+    {
+        if (!empty($this->input->post('btn_search'))) {
+            $search_name = $this->input->post('search_client_name', true);
+
+            $search_data = [
+                'name' => $search_name
+            ];
+
+            $result = array();
+            $result['data'] = $this->ClientTable_model->search_client_name($search_data);
+            $result['shop_name'] =$this->ClientTable_model->get_shop_name();
+            $this->load->view('ClientTable_view', $result);
+        }
+    }
 }
