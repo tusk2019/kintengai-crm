21fefaf31a8764165af0c1eddafa13653c2b83c4
tusk2019 <53996136+tusk2019@users.noreply.github.com>
2020-05-07 18:09:18 +0900

    テーブル画面へのリンク先URLのhttpをhttpsに変更

--- application/views/Clientedit_view.php
+++ application/views/Clientedit_view.php
@@ -55,7 +55,7 @@ function h($str)
     <!--row-->
     <div class="row">
         <div class="col-12">
-          <a href="http://kintengai.booktown.tokyo/index.php/ClientTableC" class="btn btn-secondary">テーブル画面に戻る</a>
+          <a href="https://kintengai.booktown.tokyo/index.php/ClientTableC" class="btn btn-secondary">テーブル画面に戻る</a>
           <input type="submit" value="変更の保存" class="btn btn-success toastsDefaultSuccess" name="btn_save" id="submitButton">
         </div>
     </div>
