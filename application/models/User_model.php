fdf48f1165b46675098d330f97a4fb5e5b1603ea
tusk2019 <53996136+tusk2019@users.noreply.github.com>
2020-05-23 12:56:13 +0900

    Fix User_model.php

--- application/models/User_model.php
+++ application/models/User_model.php
@@ -7,12 +7,6 @@ class User_model extends CI_Model
         parent::__construct();
     }
 
-    public function get_record()
-    {
-        $query = $this->db->get('user')->result_array();
-        return $query;
-    }
-
     public function get_by_email($email)
     {
         $query = $this->db->get_where('user', array('email' => $email));
